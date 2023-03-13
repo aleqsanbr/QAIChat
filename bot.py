4a3aca910b7227e3fd83ca1e07427c91654a0810
aleqsanbr <abs301216@gmail.com>
2023-03-13 12:04:09 +0300

    Fix saving context mode

--- bot.py
+++ bot.py
@@ -3,7 +3,8 @@ import openai
 
 keylist = {}
 context_on = {}
-previous_msgs = {}
+msgs = {}
+history_debug = {}
 bot = telebot.TeleBot("", parse_mode=None, skip_pending=True)
 
 
@@ -13,13 +14,13 @@ def send_welcome(message):
                  "Привет! Это QAI Chat! Я с удовольствием помогу вам использовать ChatGPT прямо в Telegram! Для более подробных инструкций введите /help",
                  parse_mode="HTML")
     bot.send_message(message.chat.id,
-                     "По умолчанию бот не запоминает историю переписки, соответственно, ChatGPT не будет ориентироваться на предыдущие сообщения. Если вы хотите включить, введите /switch_context_on.")
+                     "По умолчанию бот запоминает историю переписки, соответственно, ChatGPT будет ориентироваться на предыдущие сообщения. Если вы хотите выключить, введите /switch_context_off.")
 
 
 @bot.message_handler(commands=['help'])
 def help(message):
     bot.reply_to(message,
-                 "<b>Инструкция по использованию бота</b>\n\n1️⃣ Зарегистрируйтесь на OpenAI. Для этого сначала ознакомьтесь с материалом (https://habr.com/ru/post/704600) на Habr (там подробно описано, как зарегистрировать виртуальный номер и прочие моменты, потому что OpenAI, вообще говоря, недоступен в России)\n\n2️⃣ После регистрации получите API-ключ на https://platform.openai.com/account/api-keys. Далее введите команду /api, чтобы сохранить его здесь (без этого бот работать не будет)\n\n3️⃣ После установки ключа пишите любое сообщение, я передам его ChatGPT и возвращу ответ\n\n<b>Обратите внимание:</b> бот пока не умеет запоминать контекст предыдущих сообщений.\n\n4️⃣По умолчанию бот не запоминает историю переписки, соответственно, ChatGPT не будет ориентироваться на предыдущие сообщения. Если вы хотите включить, введите /switch_context_on. В таком случае бот перейдет в режим продолжения диалога и по API будет передаваться вся история переписки, чтобы модель могла распознавать контекст. Это может работать нестабильно. Чтобы очистить сохраненную переписку, введите /context_reset. Чтобы выйти из режима, введите /switch_context_off (это сохранит переписку, но бот обращаться к ней не будет).",
+                 "<b>Инструкция по использованию бота</b>\n\n1️⃣ Зарегистрируйтесь на OpenAI. Для этого сначала ознакомьтесь с материалом (https://habr.com/ru/post/704600) на Habr (там подробно описано, как зарегистрировать виртуальный номер и прочие моменты, потому что OpenAI, вообще говоря, недоступен в России)\n\n2️⃣ После регистрации получите API-ключ на https://platform.openai.com/account/api-keys. Далее введите команду /api, чтобы сохранить его здесь (без этого бот работать не будет)\n\n3️⃣ После установки ключа пишите любое сообщение, я передам его ChatGPT и возвращу ответ\n\n<b>Обратите внимание:</b> бот пока не умеет запоминать контекст предыдущих сообщений.\n\n4️⃣ По умолчанию бот запоминает историю переписки, соответственно, ChatGPT будет ориентироваться на предыдущие сообщения. Если вы хотите выключить, введите /switch_context_off. В таком случае бот выйдет из режима продолжения диалога и по API не будет передаваться вся история переписки. Чтобы очистить сохраненную переписку, введите /context_reset. Чтобы войти из режима, введите /switch_context_on.",
                  parse_mode="HTML")
 
 
@@ -66,48 +67,47 @@ def switch_context_off(message):
 @bot.message_handler(commands=['context_reset'])
 def context_reset(message):
     bot.reply_to(message,
-                 f"История переписки сброшена. ChatGPT забыл все, статус запоминания контекста: {context_on.get(message.chat.id, False)}. {'Продолжайте переписку, чтобы бот начал запоминать историю. Выключить режим: /switch_context_off' if context_on.get(message.chat.id, False) else 'Чтобы включить режим запоминания контекста, введите /switch_context_on'}",
+                 f"История переписки сброшена. ChatGPT забыл все, статус запоминания контекста: {context_on.get(message.chat.id, True)}. {'Продолжайте переписку, чтобы бот начал запоминать историю. Выключить режим: /switch_context_off' if context_on.get(message.chat.id, True) else 'Чтобы включить режим запоминания контекста, введите /switch_context_on'}",
                  parse_mode="HTML")
-    previous_msgs[message.chat.id] = ""
+    msgs.pop(message.chat.id, None)
 
 
 @bot.message_handler(commands=['debug'])
 def debug(message):
     bot.reply_to(message,
-                 str(context_on.get(message.chat.id, False)) + " /// " + str(message.chat.id) + " /// " + keylist.get(
+                 str(context_on.get(message.chat.id, True)) + " /// " + str(message.chat.id) + " /// " + keylist.get(
                      message.chat.id, "None"))
-    bot.reply_to(message, previous_msgs[message.chat.id])
+    history_debug[message.chat.id] = ""
+    for i in msgs[message.chat.id]:
+        history_debug[message.chat.id] += i["role"] + ": " + i["content"] + "\n\n"
+    bot.reply_to(message, history_debug[message.chat.id])
+    print(msgs[message.chat.id])
 
 
 @bot.message_handler(func=lambda message: True)
 def ask(message):
     try:
-        global previous_msgs
+        global msgs
         if message.chat.id not in keylist.keys():
             bot.reply_to(message, "Вы не установили ключ. Введите /api для того, чтобы установить")
         else:
             loading = bot.reply_to(message, "Отправляю запрос...")
             openai.api_key = keylist[message.chat.id]
-            if context_on.get(message.chat.id, False):
-                what_are_you_asking = previous_msgs.get(message.chat.id,
-                                                        "") + f"\n\n{message.from_user.first_name}: " + (
-                                          (message.text + ".") if message.text[-1] not in ".!?" else message.text)
+            if context_on.get(message.chat.id, True):
+                msgs[message.chat.id] = list(msgs.get(message.chat.id, {}))
+                msgs[message.chat.id].append({"role": "user", "content": message.text})
+                what_are_you_asking = msgs[message.chat.id]
             else:
-                what_are_you_asking = message.text
-            query = (
-                    "Добавь сюда только одну реплику собеседника 'ChatGPT'. \n\n\n" + what_are_you_asking) if context_on.get(
-                message.chat.id, False) else what_are_you_asking
+                what_are_you_asking = [{"role": "user", "content": message.text}]
             completion = openai.ChatCompletion.create(
-                model="gpt-3.5-turbo",
-                messages=[
-                    {"role": "user",
-                     "content": query}
-                ]
+                model = "gpt-3.5-turbo",
+                messages = what_are_you_asking
             )
             openai.api_key = None
             current_reply = completion.choices[0].message.content
-            if context_on.get(message.chat.id, False):
-                previous_msgs[message.chat.id] = what_are_you_asking + "\n\nChatGPT: " + current_reply
+            if context_on.get(message.chat.id, True):
+                msgs[message.chat.id] = list(msgs.get(message.chat.id, {}))
+                msgs[message.chat.id].append({"role": "assistant", "content": current_reply})
             bot.delete_message(message.chat.id, loading.message_id)
             bot.reply_to(message, current_reply, parse_mode=None)
     except Exception as e:
